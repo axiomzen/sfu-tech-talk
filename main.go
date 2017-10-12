de7b2d49350ba46709193ec3b64af598bc037d10
Howard <hwrdtm@users.noreply.github.com>
2017-10-12 15:28:26 -0700

    authorization and cors

--- main.go
+++ main.go
@@ -27,6 +27,25 @@ func main() {
 
 	mux := powermux.NewServeMux()
 
+	mux.Route("/").OptionsFunc(func(w http.ResponseWriter, _ *http.Request) {
+		w.Header().Add("Access-Control-Allow-Origin", "*")
+		w.Header().Add("Access-Control-Allow-Method", "GET,POST")
+		w.Header().Add("Access-Control-Allow-Header", "Authorization")
+	}).MiddlewareFunc(func(w http.ResponseWriter, r *http.Request, n func(w http.ResponseWriter, _ *http.Request)) {
+		w.Header().Add("Access-Control-Allow-Origin", "*")
+		n(w, r)
+	})
+
+	mux.Route("/").MiddlewareFunc(func(w http.ResponseWriter, r *http.Request, n func(w http.ResponseWriter, _ *http.Request)) {
+		authToken := r.Header.Get("Authorization")
+		if authToken == "sfu" {
+			n(w, r)
+			return
+		}
+
+		w.WriteHeader(http.StatusUnauthorized)
+	})
+
 	mux.Route("/ping").GetFunc(func(w http.ResponseWriter, _ *http.Request) {
 		io.WriteString(w, "pong")
 	})
