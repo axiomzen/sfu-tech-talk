51988f086f987d0a8a58fb67c187507d639f658b
Howard <hwrdtm@users.noreply.github.com>
2017-10-12 15:02:41 -0700

    setup models

---
+++ models.go
@@ -0,0 +1,38 @@
+package main
+
+import "github.com/go-pg/pg"
+
+// Question model
+type Question struct {
+	ID      int    `json:"id"`
+	Text    string `json:"text"`
+	Author  string `json:"author"`
+	Upvotes int    `json:"upvotes"`
+}
+
+// Data Access Layer
+type DAL struct {
+	conn *pg.DB
+}
+
+func (dal *DAL) AddQuestion(q *Question) error {
+	_, err := dal.conn.Model(q).Insert()
+	return err
+}
+
+func (dal *DAL) GetQuestion(id int) (*Question, error) {
+	question := &Question{}
+	err := dal.conn.Model(question).Where("id = ?", id).Select()
+	return question, err
+}
+
+func (dal *DAL) GetQuestions() ([]*Question, error) {
+	questions := make([]*Question, 0)
+	err := dal.conn.Model(questions).Select()
+	return questions, err
+}
+
+func (dal *DAL) Upvote(id int) error {
+	_, err := dal.conn.Model(&Question{}).Where("id = ?", id).Set("upvotes = upvotes + 1").Update()
+	return err
+}
