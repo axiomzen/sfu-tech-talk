e689bfbcc32a3ca4fc05ea600792f6c321552187
Howard <hwrdtm@users.noreply.github.com>
2017-10-16 11:09:45 -0700

    Add auth middleware

--- main.go
+++ main.go
@@ -12,6 +12,16 @@ func main() {
 
 	mux := powermux.NewServeMux()
 
+	mux.Route("/").MiddlewareFunc(func(w http.ResponseWriter, r *http.Request, n func(w http.ResponseWriter, r *http.Request)) {
+		authToken := r.Header.Get("Authorization")
+		if authToken == "sfu" {
+			n(w, r)
+			return
+		}
+
+		w.WriteHeader(http.StatusUnauthorized)
+	})
+
 	mux.Route("/ping").GetFunc(func(w http.ResponseWriter, r *http.Request) {
 		io.WriteString(w, "pong")
 	})
