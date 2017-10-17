aabfefcb48377b227007ce56f7891b91e99118c0
Howard <hwrdtm@users.noreply.github.com>
2017-10-17 16:15:08 -0700

    post question

--- handler.go
+++ handler.go
@@ -10,6 +10,12 @@ type QuestionHandler struct {
 	db *DAL
 }
 
+func decode(r io.Reader, obj interface{}) error {
+	decoder := json.NewDecoder(r)
+	err := decoder.Decode(obj)
+	return err
+}
+
 func render(w io.Writer, obj interface{}) error {
 	encoder := json.NewEncoder(w)
 	err := encoder.Encode(obj)
@@ -32,7 +38,24 @@ func (h *QuestionHandler) GetQuestions(w http.ResponseWriter, r *http.Request) {
 
 func (h *QuestionHandler) AddQuestion(w http.ResponseWriter, r *http.Request) {
 
+	question := &Question{}
+
+	if err := decode(r.Body, question); err != nil {
+		http.Error(w, "Bad request", http.StatusBadRequest)
+		return
+	}
+
+	if err := h.db.AddQuestion(question); err != nil {
+		http.Error(w, "Database error", http.StatusInternalServerError)
+		return
+	}
+
+	if err := render(w, question); err != nil {
+		http.Error(w, "Render error", http.StatusInternalServerError)
+		return
+	}
 }
+
 func (h *QuestionHandler) Upvote(w http.ResponseWriter, r *http.Request) {
 
 }
