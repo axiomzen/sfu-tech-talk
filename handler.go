ec6d0bf10704f988733b207a036d07d4608245c7
Howard <hwrdtm@users.noreply.github.com>
2017-10-17 16:07:19 -0700

    get questions route

---
+++ handler.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+)
+
+type QuestionHandler struct {
+	db *DAL
+}
+
+func render(w io.Writer, obj interface{}) error {
+	encoder := json.NewEncoder(w)
+	err := encoder.Encode(obj)
+	return err
+}
+
+func (h *QuestionHandler) GetQuestions(w http.ResponseWriter, r *http.Request) {
+
+	questions, err := h.db.GetQuestions()
+	if err != nil {
+		http.Error(w, "Database error", http.StatusInternalServerError)
+		return
+	}
+
+	if err := render(w, questions); err != nil {
+		http.Error(w, "Render error", http.StatusInternalServerError)
+		return
+	}
+}
+
+func (h *QuestionHandler) AddQuestion(w http.ResponseWriter, r *http.Request) {
+
+}
+func (h *QuestionHandler) Upvote(w http.ResponseWriter, r *http.Request) {
+
+}
