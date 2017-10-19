c8e97a94df0a37c30eb00560d8639ed50df3cfb1
Andrew Burian <andrew@axiomzen.co>
2017-10-18 18:27:02 -0700

    Add question

--- handler.go
+++ handler.go
@@ -16,6 +16,12 @@ func render(w io.Writer, obj interface{}) error {
 	return err
 }
 
+func decode(r io.Reader, obj interface{}) error {
+	decoder := json.NewDecoder(r)
+	err := decoder.Decode(obj)
+	return err
+}
+
 func (h *QuestionHandler) GetQuestions(w http.ResponseWriter, r *http.Request) {
 
 	questions, err := h.db.GetQuestions()
@@ -32,6 +38,22 @@ func (h *QuestionHandler) GetQuestions(w http.ResponseWriter, r *http.Request) {
 
 func (h *QuestionHandler) AddQuestion(w http.ResponseWriter, r *http.Request) {
 
+	question := &Question{}
+
+	if err := decode(r.Body, question); err != nil {
+		http.Error(w, "Bad request", http.StatusBadRequest)
+		return
+	}
+
+	if err := h.db.AddQuestion(question); err != nil {
+		http.Error(w, "Database Error", http.StatusInternalServerError)
+		return
+	}
+
+	if err := render(w, question); err != nil {
+		http.Error(w, "Render error", http.StatusInternalServerError)
+		return
+	}
 }
 
 func (h *QuestionHandler) Upvote(w http.ResponseWriter, r *http.Request) {
